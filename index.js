3d4fc2eeb2f389f635c8a0fd605c3e857ca3633b
Saad7890-web <omysaadislam@gmail.com>
2025-11-29 13:34:45 +0600

    creating the server

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const app = {};
+
+app.config = {
+    port: 3000,
+};
+
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log(`Server is listening on port ${app.config.port}`);
+    })
+};
+
+app.handleReqRes = (req, res) => {
+    res.end('hello');
+};
+
+app.createServer();
